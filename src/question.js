1732af4493046adb1964fc6ad021d436520e6daa
Dmytro Shevchenko <dimashevchenko10021999@gmail.com>
2020-06-16 09:07:03 +0300

    Add auth token. Finish develop project
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Shevchenko Dmytro <dimashevchenko10021999@gmail.com>

--- src/question.js
+++ src/question.js
@@ -16,14 +16,40 @@ export class Question {
             .then(Question.renderList)
     }
 
+    static fetch(token) {
+        if (!token) {
+            return Promise.resolve('<p class="error">У вас нет токена</p>')
+        }
+        return fetch(`https://question-js-aeffa.firebaseio.com/questions.json?auth=${token}`)
+            .then(response => response.json())
+            .then(response => {
+                if (response && response.error) {
+                    return `<p class="error">${response.error}</p>`
+                }
+
+                return response ? Object.keys(response).map(key => ({
+                    ...response[key],
+                    id: key
+                })) : []
+            })
+    }
+
     static renderList() {
         const questions = getQuestionsFromLocalStorage()
+
         const html = questions.length
             ? questions.map(toCard).join('')
-            : `<div class="mui--text-headline">Вы пока ничего не спрашывали!</div>`
+            : `<div class="mui--text-headline">Вы пока ничего не спрашивали</div>`
 
         const list = document.getElementById('list')
+
         list.innerHTML = html
+    }
+
+    static listToHTML(questions) {
+        return questions.length
+            ? `<ol>${questions.map(q => `<li>${q.text}</li>`).join('')}</ol>`
+            : '<p>Вопросов пока нет</p>'
     }
 }
 
@@ -33,18 +59,17 @@ function addToLocalStorage(question) {
     localStorage.setItem('questions', JSON.stringify(all))
 }
 
-
 function getQuestionsFromLocalStorage() {
     return JSON.parse(localStorage.getItem('questions') || '[]')
 }
 
 function toCard(question) {
     return `
-        <div class="mui--text-black-54">
-            ${new Date(question.date).toLocaleDateString()}
-            ${new Date(question.date).toLocaleTimeString()}
-        </div>
-        <div>${question.text}</div>
-        <br>
+      <div class="mui--text-black-54">
+        ${new Date(question.date).toLocaleDateString()}
+        ${new Date(question.date).toLocaleTimeString()}
+      </div>
+      <div>${question.text}</div>
+      <br>
     `
 }
